1f99a5777a058e204e84cf2a4dd1439723446012
IP Update Automation <automation@radiosilence.dev>
2024-12-26 22:50:59 +0000

    hurr

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "jaritanet_tunnel_cname" {
-  value       = jaritanet_tunnel.cname
+  value       = module.jaritanet_tunnel.cname
   description = "The CNAME record that routes to the JARITANET tunnel."
 }
 
 output "jaritanet_tunnel_token" {
-  value       = jaritanet_tunnel.token
+  value       = module.jaritanet_tunnel.token
   description = "The token to use for the JARITANET tunnel."
 }
