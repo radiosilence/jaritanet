20dc1cfcbd7a97e0c5c410bd8e77add160f7b07d
James Cleveland <jc@blit.cc>
2025-01-05 18:48:38 +0000

    adding lots of oocs

--- pulumi/src/types.ts
+++ pulumi/src/types.ts
@@ -1,7 +1,16 @@
+/**
+ * Configuration for the Cloudflare provider.
+ */
 export interface CloudflareConf {
+  /**
+   * Cloudflare account ID.
+   */
   accountId: string;
 }
 
+/**
+ * Configuration for Fastmail DNS records.
+ */
 export interface FastmailConf {
   mxDomain: string;
   dkimDomain: string;
@@ -12,24 +21,64 @@ export interface FastmailConf {
   spfDomain: string;
 }
 
+/**
+ * Configuration for Bluesky DNS records.
+ */
 export interface BlueskyConf {
+  /**
+   * Bluesky requires a DID to be set in the DNS records for authentication.
+   */
   did: string;
 }
 
+/**
+ * Configuration for a tunneled service.
+ */
 export interface ServiceConf {
+  /**
+   * Record name for the service.
+   */
   name: string;
+
+  /**
+   * Internal service URL.
+   */
   service: string;
 }
 
+/**
+ * Current types of DNS modules.
+ */
 type DnsModule = "bluesky" | "fastmail";
 
+/**
+ * Configuration for a Cloudflare tunnel.
+ */
 export interface TunnelConf {
   name: string;
 }
 
+/**
+ * Configuration for a zone.
+ */
 export interface ZoneConf {
+  /**
+   * The Cloudflare ID of the zone.
+   */
   zoneId: string;
+
+  /**
+   * The name of the zone.
+   */
   name: string;
+
+  /**
+   * DNS modules to apply.
+   */
   modules: DnsModule[];
+
+  /**
+   * Services to tunnel.
+   */
   services?: ServiceConf[];
 }
