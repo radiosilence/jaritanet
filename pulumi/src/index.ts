ada94500ae0c21674986d4936963d34af6ee4be5
James Cleveland <jc@blit.cc>
2025-01-05 17:54:28 +0000

    more succinct

--- pulumi/src/index.ts
+++ pulumi/src/index.ts
@@ -5,11 +5,10 @@ import * as pulumi from "@pulumi/pulumi";
 const config = new pulumi.Config();
 
 const zones = config.requireObject<ZoneConf[]>("zones");
-const tunnelConfig = config.requireObject<TunnelConf>("tunnel");
 
 const jaritanetTunnel = tunnel.cloudflareTunnel({
-  ...tunnelConfig,
-  zones: Object.values(zones),
+  ...config.requireObject<TunnelConf>("tunnel"),
+  zones,
 });
 
 for (const zone of zones) {
