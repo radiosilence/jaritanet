d48023409d7f37c6813b609997dc151d4336692c
James Cleveland <jc@blit.cc>
2025-01-06 19:40:25 +0000

    adding to k8s pulumi

--- k8s/src/index.ts
+++ k8s/src/index.ts
@@ -1,7 +1,13 @@
 import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
+import { createLocalServer, type LocalServerArgs } from "./lib/local-server";
+
+const config = new pulumi.Config();
 
 export const namespace = new k8s.core.v1.Namespace("main", {
   metadata: {
     name: "main",
   },
 });
+
+createLocalServer("files", config.requireObject<LocalServerArgs>("files"));
