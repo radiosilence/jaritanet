546f91b66a5a294dc05fdf4dcf2e40302a94cdd6
James Cleveland <jc@blit.cc>
2025-01-09 20:42:22 +0000

    hmm,

--- k8s/src/templates/service.ts
+++ k8s/src/templates/service.ts
@@ -60,6 +60,9 @@ export function createService(
                     readOnly,
                   })
                 ),
+                securityContext: {
+                  allowPrivilegeEscalation: false,
+                },
               },
             ],
           },
