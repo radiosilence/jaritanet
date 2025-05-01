28538a72f6849c75f98359444b7a41298c3dc569
James Cleveland <james.cleveland@fresha.com>
2025-05-01 08:37:16 +0100

    use int type

--- packages/k8s/src/templates/cloudflared.schemas.ts
+++ packages/k8s/src/templates/cloudflared.schemas.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
 
 export const CloudflaredArgsSchema = z.object({
-  replicas: z.number(),
+  replicas: z.uint32(),
 });
--- packages/k8s/src/templates/service.schemas.ts
+++ packages/k8s/src/templates/service.schemas.ts
@@ -9,9 +9,9 @@ import {
 
 export const ServiceArgsSchema = z.object({
   image: ImageSchema,
-  replicas: z.number().default(1),
+  replicas: z.uint32().default(1),
   env: z.record(z.string(), z.string()).default({}),
-  httpPort: z.number().default(80),
+  httpPort: z.uint32().default(80),
   limits: LimitsSchema.default(LIMITS_DEFAULT),
   hostVolumes: z.array(HostVolumeSchema).default([]),
   persistence: z.array(PersistenceSchema).default([]),
