609e9c429e9eb7ebd33a46eb36237f789e6150fa
James Cleveland <james.cleveland@fresha.com>
2025-06-11 17:12:51 +0100

    retain type safety

--- packages/infra/src/conf.ts
+++ packages/infra/src/conf.ts
@@ -1,9 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
+import { z } from "zod/v4";
 import { InfraConfSchema } from "./conf.schemas.ts";
 
 const config = new pulumi.Config();
 
-export const conf = InfraConfSchema.parse({
+const { data, error } = InfraConfSchema.safeParse({
   cloudflare: config.requireObject("cloudflare"),
   tunnel: config.requireObject("tunnel"),
 });
+
+if (error) {
+  throw new Error(`Could not parse config:\n\n${z.prettifyError(error)}\n\n`);
+}
+
+export const conf = data;
