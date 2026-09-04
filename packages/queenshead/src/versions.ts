5b6cd56f37834097e90701e7341b43849763d9d2
GitHub Action <action@github.com>
2026-09-04 19:10:09 +0000

    chore: update queenshead to sha-27017fb

--- packages/queenshead/src/versions.ts
+++ packages/queenshead/src/versions.ts
@@ -8,5 +8,5 @@
  * Rewritten in place by the version updater; see `.github/tracked-versions.yml`.
  */
 export const VERSIONS = {
-  queenshead: "sha-90ffbad",
+  queenshead: "sha-27017fb",
 } as const;
